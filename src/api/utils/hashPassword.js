f16020462035450e5e12575f12df08003ecc1688
Tran Dai Quy <trandaiquy2903@gmail.com>
2023-01-10 22:27:33 +0700

    add hashPassword util

---
+++ src/api/utils/hashPassword.js
@@ -0,0 +1,14 @@
+import bcrypt from 'bcryptjs';
+
+const hashPassword = async password => {
+	const salt = await bcrypt.genSalt(10);
+	const hashPassword = await bcrypt.hash(password, salt);
+	return hashPassword;
+};
+
+const validPassword = async (password, hashPassword) => {
+	const valid = await bcrypt.compare(password, hashPassword);
+	return valid;
+};
+
+export { hashPassword, validPassword };
